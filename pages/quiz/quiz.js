5f04eb3eb353da1a3465c73d1d5bd874aac2d445
Andre  Correa <andre.abc09102002@gmail.com>
2024-06-03 18:38:40 +0000

    adicionada a funcao de validar a resposta

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -9,6 +9,8 @@ let pontos = 0
 let pergunta = 1 
 let resposta = ""
 let idInputResposta = ""
+let respostaCorretaId = ""
+
 
 botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema)
@@ -104,6 +106,19 @@ function alterarSinais(texto){
 function guardarResposta(evento){
     resposta = evento.target.value
     idInputResposta = evento.target.id
+
+    const botaoEnviar=document.querySelector(".alternativas button")
+    botaoEnviar.addEventListener("click", validarResposta)
+}
+
+function validarResposta(){
+    if(resposta === quiz.questions[pergunta-1].answer){
+            document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "correta")
+            pontos = pontos + 1
+        }else{
+            document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "errada")
+            document.querySelector(`label[for='${respostaCorretaId}']`).setAttribute("id", "correta")
+        }
 }
 
 
@@ -116,6 +131,10 @@ async function iniciar(){
     const inputsResposta = document.querySelectorAll(".alternativas input")
     inputsResposta.forEach(input => {
         input.addEventListener("click", guardarResposta)
+
+        if(input.value === quiz.questions[pergunta-1].answer){
+            respostaCorretaId = input.id 
+        }
     })
 }
 
